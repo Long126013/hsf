14d3fd0b20d2eec3f3835e15dee8ebdff98056b3
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-21 00:25:10 +0700

    Update CartController.java
    add viewCart

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,4 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CartDTO;
+import com.example.demo.model.Cart;
+import com.example.demo.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
 public class CartController {
+    private final CartService cartService;
+
+    @GetMapping
+    public String viewCart(Model model) {
+        Cart cart = cartService.getCartByUserId();
+
+        model.addAttribute("cart", cart);
+        model.addAttribute("cartItems", cart.getItems());
+
+        return "cart";
+    }
 }
