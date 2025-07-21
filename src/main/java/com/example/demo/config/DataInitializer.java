1cc0b1e04d7c2bc5a25fd8134a9a3a5477d3e622
Cecil <daohaonhien@gmail.com>
2025-07-21 12:28:50 +0700

    Update DataInit

--- src/main/java/com/example/demo/config/DataInitializer.java
+++ src/main/java/com/example/demo/config/DataInitializer.java
@@ -1,16 +1,21 @@
 package com.example.demo.config;
 
+import com.example.demo.model.Product;
 import com.example.demo.model.User;
+import com.example.demo.service.ProductService;
 import com.example.demo.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @AllArgsConstructor
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private UserService userService;
+    private ProductService productService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -21,6 +26,27 @@ public class DataInitializer implements CommandLineRunner {
         userService.createUser(new User("ngoctrinh@example.com", "null", 0));
         userService.createUser(new User("t","t", 0));
 
-        System.out.println("Initialized users with encoded passwords!");
+        log.info("User initialization complete");
+
+        productInit();
+    }
+
+    public void productInit(){
+        try{
+            Product product1 = new Product("Cardboard Box", 100, 20);
+            Product product2 = new Product("Grenade (fake)", 50, 200);
+            Product product3 = new Product("Samsung Galaxy", 1, 10_000);
+            Product product4 = new Product("Pirated Minecraft", 4, 10);
+
+            productService.createProduct(product1);
+            productService.createProduct(product2);
+            productService.createProduct(product3);
+            productService.createProduct(product4);
+
+            log.info("Product initialization complete");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Error while initializing product",e.getMessage());
+        }
     }
 }
