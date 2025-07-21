f50a91e74849b6d122760b51a508741662706fc5
Huỳnh Hoàng Long <long230205@gmail.com>
2025-07-21 10:28:49 +0700

    Update CartController.java
    Fix RestController to Controller

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -5,13 +5,14 @@ import com.example.demo.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
